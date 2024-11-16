ae0e49ba65384c902a5f9f6d27c5c68d88d67881
Paulo Henrique <henriqueqx@gmail.com>
2024-11-16 17:05:40 -0300

    Algumas informações na tela inicial e relatórios

--- utils/format.ts
+++ utils/format.ts
@@ -1,6 +1,17 @@
 export const formatCurrency = (value: number): string => {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL'
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
   }).format(value);
-}; 
+};
+
+export const formatDate = (date: Date): string => {
+  return new Intl.DateTimeFormat("pt-BR").format(date);
+};
+
+export const formatDateTime = (date: Date): string => {
+  return new Intl.DateTimeFormat("pt-BR", {
+    dateStyle: "short",
+    timeStyle: "short",
+  }).format(date);
+};
